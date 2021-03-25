0edc7ba0f5c87983fe9d165be1b058ccc53cc3a0
hemant394 <cs18b014@gmail.com>
2021-03-25 19:52:53 +0530

    update
    update

--- extension.js
+++ extension.js
@@ -10,33 +10,38 @@ const xmlParser = require("fast-xml-parser")
  */
 async function activate(context) {
 
+	// const error_ = await vscode.window.onDidWriteTerminalData((e) => {return e.data})
+	// 	console.log(error_);
+
   let disposable = vscode.commands.registerCommand(
     "search-c---error.SearchSelectedText",
      async function () {
 
 		const text = editor.document.getText(editor.selection);
-		console.log(text);
+		
 		var url = 'https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=relevance&accepted=True&answers=1&site=stackoverflow';
 		url += '&q=' + encodeURIComponent(text);
 		url += '&tagged=' + encodeURIComponent("c++");
 		var res = await fetch(url);
 		var json = await res.json();
-
+		console.log(json)
 		if (json == null) return
-		
+		console.log(text);
 		const articles = json.items.map(article => {
 			return {
 			label: article.title,
-			detail: article.score,
+			detail: article.link,
 			link: article.link,
 			}
 		}  )
+
+		console.log(articles);
+		//const article = articles[0];
 		
-		const article = articles[0];
-		
-    //   const article = await vscode.window.showQuickPick(articles, {
-    //     canPickMany: true,
-    //   })
+
+      const article = await vscode.window.showQuickPick(articles, {
+        matchOnDetail: true,
+      })
 
       	if (article == null) return
       	vscode.env.openExternal(article.link)
---
+++ t.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> a;
+    a.push(12);
+    stack
+    queue
+    // for (int i = 1; i <= 5; i++)
+    //     g1.push_back(i);
+  
+    // cout << "Output of begin and end: ";
+    // for (auto i = g1.begin(); i != g1.end(); ++i)
+    //     cout << *i << " ";
+  
+    return 0;
+}
