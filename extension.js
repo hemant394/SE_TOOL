bc5f4283d0a8bae923c3c0b77f913592cb4b4d10
hemant394 <cs18b014@gmail.com>
2021-04-24 06:06:31 +0530

    last commits

--- extension.js
+++ extension.js
@@ -4,8 +4,7 @@ const editor = vscode.window.activeTextEditor;
 const term = vscode.window.activeTerminal;
 // const cons_ed = vscode.window.activeDebugConsole;
 const fetch = require('node-fetch');
-// const axios = require("axios")
-// const xmlParser = require("fast-xml-parser")
+
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -33,6 +32,23 @@ async function activate(context) {
 		// 	console.log("no terminal");
 		// }
 		// console.log(vscode.debug.activeDebugConsole.items)
+		// const articles = json.items.map(article => {
+		// 	return {
+		// 	label: article.title,
+		// 	detail: article.link,
+		// 	link: article.link,
+		// 	}
+		// }  )
+		// const panel = vscode.window.createWebviewPanel('stack',"stackoverflow view",vscode.ViewColumn.Two ,{
+		// 	enableScripts:true
+		// })
+	
+	
+		// panel.webview.html = getWebviewContentofstackoverflow( articles );
+
+
+
+
 		if (json == null) return
 		const articles = json.items.map(article => {
 			return {
@@ -59,6 +75,7 @@ async function activate(context) {
 
 		const text = editor.document.getText(editor.selection);
 		var url = 'https://api.github.com/search/repositories?sort=stars&order=desc&q='+encodeURIComponent(text)+'language:c%2B%2B';
+		
 		var res = await fetch(url);
 		var json = await res.json();
 		console.log(json);
@@ -81,9 +98,77 @@ async function activate(context) {
 
     }
   )
+  
+  let disposable3 = vscode.commands.registerCommand(
+    "search-c---error.SearchinCppreference",
+     async function () {
 
+		const text = editor.document.getText(editor.selection);
+		var url = 'http://api.serpstack.com/search?access_key=9bd922f9fff3d690c46a4efbdd37b1fc&query='+encodeURIComponent(text)+'+in+cppreference';
+		
+		var res = await fetch(url);
+		var json = await res.json();
+		
+		if (json == null) return;
+		console.log(json.organic_results);
+		const articles = json.organic_results.map(article => {
+			return {
+			label: article.title,
+			detail: article.url,
+			link: article.url,
+			}
+		}  )
+    //   const article = await vscode.window.showQuickPick(articles, {
+    //     matchOnDetail: true,
+    //   })
+
+	console.log(articles);
+	if (articles == null) return
+	  let article;
+	  if(articles.length > 0){
+		article = articles[0];
+	  }
+      	if (article == null) return
+      	vscode.env.openExternal(article.link)
+		console.log(json.items);	 
+
+    }
+  )
+
+  let disposable4 = vscode.commands.registerCommand(
+    "search-c---error.SearchinGoogle",
+     async function () {
+		const panel = vscode.window.createWebviewPanel('stack',"stackoverflow view",vscode.ViewColumn.Two ,{
+			enableScripts:true
+		})
+	
+		// console.log(view.getView())
+	
+		panel.webview.html = getWebviewContentofgoogle();
+
+    }
+  )
+
+  let stackOverflowView = vscode.commands.registerCommand(
+	  'search-c---error.GithubResults',()=>{
+	const panel = vscode.window.createWebviewPanel('GithubResults',"stackoverflow view",vscode.ViewColumn.Two ,{
+		enableScripts:true
+	})
+
+	// console.log(view.getView())
+
+	panel.webview.html = getWebviewContentofgithub();
+
+})
+
+
+
+
+  context.subscriptions.push(stackOverflowView);
   context.subscriptions.push(disposable1)
   context.subscriptions.push(disposable2)
+  context.subscriptions.push(disposable3)
+  context.subscriptions.push(disposable4)
 }
 exports.activate = activate
 
@@ -93,3 +178,74 @@ module.exports = {
   activate,
   deactivate,
 }
+
+function getWebviewContentofgithub( ) {
+	return `<!DOCTYPE html>
+	<html>
+		<head>
+			<title>SearchBar</title>
+			<style>
+				body {
+					
+				}
+			</style>
+		</head>
+		<body>
+			<h1 style="text-align: center;">Github</h1>
+			<script async src="https://cse.google.com/cse.js?cx=4cd426893c80efd6d"></script>
+<div class="gcse-search"></div>
+		</body>
+	</html>`;
+  }
+
+
+function getWebviewContentofgoogle( ) {
+	return `<!DOCTYPE html>
+	<html>
+		<head>
+			<title>SearchBar</title>
+			<style>
+				body {
+					
+				}
+			</style>
+		</head>
+		<body>
+			<h1 style="text-align: center;">Google</h1>
+			<script async src=
+	"https://cse.google.com/cse.js?cx=000888210889775888983:y9tkcjel090">
+			</script>
+			<div class="gcse-search"></div>
+		</body>
+	</html>`;
+  }
+
+
+
+  
+// function getWebviewContentofstackoverflow( article ) {
+// 	console.log(article[0]);
+// 	return `<!DOCTYPE html>
+// 	<html>
+// 		<head>
+// 			<title>SearchBar</title>
+// 			<style>
+// 				body {
+					
+// 				}
+// 			</style>
+// 		</head>
+// 		<body>
+// 			<h1 style="text-align: center;">StackOverflow</h1>
+// 			<p>Results:</p>
+// 			<ul>
+// 				<li>${article[0].label}</li>
+// 				<li><a href="${article[0].link}">${article[0].link}</a></p></li>
+// 			</ul>
+			
+// 		</body>
+// 	</html>`;
+//   }
+
+
+  
